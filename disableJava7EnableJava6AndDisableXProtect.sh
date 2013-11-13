e12d3cfd81f41e84726c8c89d785b357082f0bb3
macmule <bentoms@btopenworld.com>
2013-11-13 17:38:25 +0000

    Update disableJava7EnableJava6AndDisableXProtect.sh

--- disableJava7EnableJava6AndDisableXProtect.sh
+++ disableJava7EnableJava6AndDisableXProtect.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#!/bin/sh
 ####################################################################################################
 #
 # This is free and unencumbered software released into the public domain.
